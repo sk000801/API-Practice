2de891f585404d571ce69488a9ed918b83253c27
sugamypapa <sk000801@naver.com>
2022-08-21 20:49:30 +0900

    feat: API 연습을 위한 controller

---
+++ practice/src/main/java/api/practice/controller/ApiController.java
@@ -0,0 +1,11 @@
+package api.practice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ApiController {
+
+//    @PostMapping("/order/practice")
+//    public
+}
